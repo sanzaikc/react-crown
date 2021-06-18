9e6366899a4bba5c85c3c111fbe156d6e24e0c23
Sanjay Khatri <sanzaikc@gmail.com>
2021-06-18 19:25:40 +0545

    sign in form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/header/header-component";
-import HomePage from "./pages/homepage/homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.components";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
