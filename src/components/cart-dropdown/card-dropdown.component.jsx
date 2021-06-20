38ad242742322a24da3211392ea87b0d10e72952
Sanjay Khatri <sanzaikc@gmail.com>
2021-06-20 11:40:00 +0545

    cart items in cart

--- src/components/cart-dropdown/card-dropdown.component.jsx
+++ src/components/cart-dropdown/card-dropdown.component.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items">{cartItems.length}</div>
+    <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
 
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
