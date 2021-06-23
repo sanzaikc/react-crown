f8a7cea6bad4b37fdf1f33a8141a212422657c92
Sanjay Khatri <sanzaikc@gmail.com>
2021-06-23 14:53:48 +0545

    removing redux from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,9 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [];
 
-if (process.env.NODE_ENV === "development") middlewares.push(logger);
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
